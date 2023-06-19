6b7a52ba1ece8fc8ed19a47869c5f8ed444888fd
KPFS <KPFS@nne.com>
2023-06-19 18:30:41 +0200

    initial implementation of sde systems

--- Simulation.py
+++ Simulation.py
@@ -19,16 +19,19 @@ from types import SimpleNamespace
 from cycler import cycler
 
 nord_colors = ['#BF616A', '#D08770', '#EBCB8B', '#A3BE8C']
-nord_theme = cycler(color = nord_colors)
+nord_theme = cycler(color=nord_colors)
 
 # %% functions
 
 # noise generator for model 2
-def stochastic_noise_gen(dt, rate, seed = 42):
+
+
+def stochastic_noise_gen(dt, rate, seed=42):
     noise = dict()
     rng = np.random.RandomState(seed)
 
     eta = stats.expon(scale=1/rate)
+
     def stochastic_noise(t):
         k = np.floor(t / dt)
         if not k in noise:
@@ -97,16 +100,52 @@ class simulation_system:
         # make observations
         self.obs = np.zeros(self.sim.y.shape)
         for i in range(len(self.sim.t)):
-            self.obs[:,i] = self.observation_eq(self.sim.t[i], self.sim.y[:,i])
+            self.obs[:, i] = self.observation_eq(
+                self.sim.t[i], self.sim.y[:, i])
         # make outputs
         self.out = np.zeros(self.sim.y.shape)
         for i in range(len(self.sim.t)):
-            self.out[:,i] = self.output_eq(self.sim.t[i], self.sim.y[:,i])
+            self.out[:, i] = self.output_eq(self.sim.t[i], self.sim.y[:, i])
 
         # assign t, and y for ease of plotting
         self.t = self.sim.t
         self.y = self.sim.y
-        
+
+class SDE_system(simulation_system):
+
+    def system_eq(self, t, x):
+        return self.A @ np.nan_to_num(np.sqrt(x)) + self.Gamma @ self.u(t)
+
+    def run_sim(self, t_span, x0, dt, seed=42):
+        # set seed for reproducibility
+        rng = np.random.RandomState(seed)
+
+        # simulate using euler-maruyama method
+        t = np.arange(t_span[0], t_span[1], dt)
+        y = np.zeros([4, len(t)])
+        obs = np.zeros(y.shape)
+        out = np.zeros(y.shape)
+
+        y[:,0] = x0
+        for i in range(1, len(t)):
+            #state update
+            yn = y[:,i-1].reshape(4,1)
+            
+            yn = yn + self.system_eq(t[i], yn)*dt + self.D @ self.d(
+                t[i]) * stats.norm(scale=dt).rvs(1, random_state=rng)
+            
+            y[:,i] = yn.reshape(4)
+            # calc observation and output
+            obs[:,i] = self.observation_eq(t[i], yn).reshape(4)
+            out[:,i] = self.output_eq(t[i], yn).reshape(4)
+        #save to object
+
+        self.t = t
+        self.y = y
+        self.obs = obs
+        self.out = out
+
+
 # %%
 Params_base_min = {
     "gamma_1": 0.58,  # split 1
@@ -143,53 +182,68 @@ det_min = simulation_system(Params_base_min,
                             u=lambda t: np.array([0, 0]).reshape(2, 1),
                             d=lambda t: np.array(
                                 [np.sin(t) + 1, np.cos(t) + 1]).reshape(2, 1),
-                            obs_noise= lambda t: np.array([0, 0, 0, 0]))
+                            obs_noise=lambda t: np.array([0, 0, 0, 0]))
 det_nonmin = simulation_system(Params_base_nonmin,
                                u=lambda t: np.array([0, 0]).reshape(2, 1),
                                d=lambda t: np.array(
                                    [np.sin(t) + 1, np.cos(t) + 1]).reshape(2,  1),
-                               obs_noise = lambda t: np.array([0, 0, 0, 0]))
+                               obs_noise=lambda t: np.array([0, 0, 0, 0]))
 stoch_min = simulation_system(Params_base_min,
-                               u=lambda t: np.array([0, 0]).reshape(2, 1),
-                               d=stochastic_noise_gen(10, 1),
-                               obs_noise=lambda t: stats.norm(scale=1).rvs(4))
+                              u=lambda t: np.array([0, 0]).reshape(2, 1),
+                              d=stochastic_noise_gen(10, 1),
+                              obs_noise=lambda t: stats.norm(scale=1).rvs(4))
 stoch_nonmin = simulation_system(Params_base_nonmin,
-                               u=lambda t: np.array([0, 0]).reshape(2, 1),
-                               d=stochastic_noise_gen(10, 1),
-                               obs_noise=lambda t: stats.norm(scale=1).rvs(4))
+                                 u=lambda t: np.array([0, 0]).reshape(2, 1),
+                                 d=stochastic_noise_gen(10, 1),
+                                 obs_noise=lambda t: stats.norm(scale=1).rvs(4))
+sde_min = SDE_system(Params_base_min, 
+                     u = lambda t: np.array([0, 0]).reshape(2, 1),
+                     d = lambda t: 3*np.array([np.sin(t) + 1, np.cos(t) + 1]).reshape(2, 1),
+                     obs_noise = lambda t: stats.norm(scale=1).rvs([4,1]))
+
+sde_nonmin = SDE_system(Params_base_nonmin, 
+                        u = lambda t: np.array([0, 0]).reshape(2, 1),
+                        d = lambda t: 3*np.array([np.sin(t) + 1, np.cos(t) + 1]).reshape(2, 1),
+                        obs_noise = lambda t: stats.norm(scale=1).rvs([4,1]))
 
 
-#%% run and plot simulations
+# %% run and plot simulations
 
 x0 = np.array([0, 0, 0, 0])
-tspan = [0, 200]
+tspan = [0, 600]
 det_min.run_sim(tspan, x0)
 det_nonmin.run_sim(tspan, x0)
 stoch_min.run_sim(tspan, x0)
 stoch_nonmin.run_sim(tspan, x0)
-
+#%%
+sde_min.run_sim(tspan, x0, dt = 0.1)
+sde_nonmin.run_sim(tspan, x0, dt = 0.1)
 
 
 # tmp function to reuse plottin, without copy paste
-def plot_systems(min_sys, nonmin_sys, ylim = [0,2]):
+def plot_systems(min_sys, nonmin_sys, ylim=[0, 2]):
 
     fig = plt.figure(figsize=(12, 6))
     ax = plt.gca()
     ax.set_prop_cycle(nord_theme)
-    plt.plot(min_sys.t, np.transpose(min_sys.out) , linestyle='-')
+    plt.plot(min_sys.t, np.transpose(min_sys.out), linestyle='-')
     plt.plot(nonmin_sys.t, np.transpose(nonmin_sys.out), linestyle='--')
-    plt.plot(0, 0, linestyle='-', color = 'k', label = 'minimum system')
-    plt.plot(0, 0, linestyle='--', color = 'k', label = 'non-minimum system')
+    plt.plot(0, 0, linestyle='-', color='k', label='minimum system')
+    plt.plot(0, 0, linestyle='--', color='k', label='non-minimum system')
     plt.ylim(ylim)
-    legend_elements = [plt.Line2D([0], [0], color = nord_colors[0], label = 'Tank 1'),
-                    plt.Line2D([0], [0], color = nord_colors[1], label = 'Tank 2'),
-                    plt.Line2D([0], [0], color = nord_colors[2], label = 'Tank 3'),
-                    plt.Line2D([0], [0], color = nord_colors[3], label = 'Tank 4'),
-                    plt.Line2D([0], [0], color = 'k', linestyle = '-', label = 'Minimum system'),
-                    plt.Line2D([0], [0], color = 'k', linestyle = '--', label = 'Non-minimum system')]
-    plt.legend(handles = legend_elements)
+    legend_elements = [plt.Line2D([0], [0], color=nord_colors[0], label='Tank 1'),
+                       plt.Line2D([0], [0], color=nord_colors[1],
+                                  label='Tank 2'),
+                       plt.Line2D([0], [0], color=nord_colors[2],
+                                  label='Tank 3'),
+                       plt.Line2D([0], [0], color=nord_colors[3],
+                                  label='Tank 4'),
+                       plt.Line2D([0], [0], color='k',
+                                  linestyle='-', label='Minimum system'),
+                       plt.Line2D([0], [0], color='k', linestyle='--', label='Non-minimum system')]
+    plt.legend(handles=legend_elements)
 
+#%%
 plot_systems(det_min, det_nonmin)
 plot_systems(stoch_min, stoch_nonmin)
-
-# %%
+plot_systems(sde_min, sde_nonmin,ylim = [-2,2])
